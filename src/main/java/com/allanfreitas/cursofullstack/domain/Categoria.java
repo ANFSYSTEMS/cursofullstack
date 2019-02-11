d35ae38b7ed4df47c8ea3a9989bf2c7f02b444c4
Allan Freitas <anfsystems@gmail.com>
2019-02-11 21:47:28 -0200

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/allanfreitas/cursofullstack/domain/Categoria.java
+++ src/main/java/com/allanfreitas/cursofullstack/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.allanfreitas.cursofullstack.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	public Categoria() {
